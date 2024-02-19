516b1bf81992c39821257aea4b1852d663036ce8
Elouan <elouanbahripro@gmail.com>
2024-02-19 20:24:16 +0100

    Mon premier réseau de neuronne à 2 couches

--- ML/nmachine_learnia.py
+++
@@ -1,9 +0,0 @@
-import math
-import numpy as np
-from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-
-X,y= make_blobs(n_samples=100,n_features=2,centers=2,random_state=0)
-
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap="summer")
-plt.show()
